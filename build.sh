1d3aff9906e42b91ae0ae1c7ee12bf3f7db03336
Benedict Allerberger <me@bene.dev>
2023-02-20 12:41:58 +0100

    Fix build script

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 # Download Tailwind CLI
 curl -sLO https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-linux-x64
-chmod +x tailwindcss-macos-arm64
+chmod +x tailwindcss-linux-x64
 
 # Build CSS
-./tailwindcss-macos-arm64 -i ./src/main.css -o ./static/main.css
+./tailwindcss-linux-x64 -i ./src/main.css -o ./static/main.css
 
 # Build status page
 go run ./src/main.go
